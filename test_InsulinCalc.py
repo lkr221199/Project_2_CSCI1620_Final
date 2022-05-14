c4d48957fca94262abf976556ed35a7b86bc254a
lkr221199 <lreichwaldt@unomaha.edu>
2022-05-13 22:00:28 -0500

    Added to test

--- test_InsulinCalc.py
+++ test_InsulinCalc.py
@@ -10,9 +10,10 @@ test_correction_calc_outcomes = [[50, 120, 60, 0],
                                  [176, 110, 50, 1.32],
                                  ]
 
-test_total_dose_calc_outcomes = [[3.66, 0, 4],
-                                 [0.05, 1.32, 1],
+test_total_dose_calc_outcomes = [[1, 1, 1, 1, 1, 1],
+                                 [100, 120, 50, 20, 1, 0]
                                  ]
+
 
 
 class TestInsulinCalc:
@@ -37,10 +38,13 @@ class TestInsulinCalc:
         self.calc.calculate_correction()
         assert self.calc.correction_dose == pytest.approx(correction_dose, abs=0.001)
 
-    @pytest.mark.parametrize('bolus_dose, correction_dose, total_insulin_dose', test_total_dose_calc_outcomes)
-    def test_calculate_total_dose(self, bolus_dose, correction_dose, total_insulin_dose):
-        self.calc.bolus = bolus_dose
-        self.calc.correction_dose = correction_dose
+    @pytest.mark.parametrize('current_blood_sugar, target_blood_sugar, correction_factor, ratio, total_carbs, total_insulin_dose', test_total_dose_calc_outcomes)
+    def test_calculate_total_dose(self, current_blood_sugar, target_blood_sugar, correction_factor, ratio, total_carbs, total_insulin_dose):
+        self.calc.current_blood_sugar = current_blood_sugar
+        self.calc.target_blood_sugar = target_blood_sugar
+        self.calc.correction_factor = correction_factor
+        self.calc.ratio = ratio
+        self.calc.total_carbs = total_carbs
         self.calc.calculate_total_dose()
         self.calc.total_insulin_dose = round(self.calc.total_insulin_dose)
         assert self.calc.total_insulin_dose == pytest.approx(total_insulin_dose, abs=0.001)
